955e62a9810981c70cf94750f498e581a26e9122
ashishkunthe <ashishkunthe2003@gmail.com>
2025-08-13 13:55:35 +0530

    some small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <title>data-alchemist</title>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
